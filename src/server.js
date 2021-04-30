67b7aed6cb4d4aee9b31a75aee77e6c42c0e3595
Ulisses Brandao <ulissespkk@gmail.com>
2021-04-30 11:30:00 -0300

    Erro no JSON

--- src/server.js
+++ src/server.js
@@ -1,15 +1,20 @@
 const express = require('express')
 const app = express()
-const porta = 3000
-app.use(express.json({extends: true}))
+const porta = 3000;
+
+app.use(express.json({extends: true}));
+const fs = require('fs')
 
 app.get('/', (req, res)=>{
     res.json({messege:'Tudo certo!'})
 })
 
 app.post('/cadastrar', (req, res)=>{
-    const {nome, preco} = req.body
-    console.log({nome, preco})
+    const {nome, preco} = req.body;
+
+    const produtos = fs.readFileSync('./data/items.json', 'utf8');
+    console.log(JSON.parse(produtos))
+
     res.send({nome, preco})
 })
 
