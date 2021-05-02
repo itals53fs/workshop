41bfa5ea19c7bebfe8282249cdb1d659a76827db
itals53fs <tales.felix.10@gmail.com>
2021-05-02 18:20:09 -0300

    ok

--- src/server.js
+++ src/server.js
@@ -4,14 +4,23 @@ const porta = 3000
 app.use(express.json({extends: true}))
 
 app.get('/', (req, res)=>{
-    res.json({messege:'Tudo certo!'})
+    res.send({messege:'Tudo certo!'})
 })
+
+
 
 app.post('/cadastrar', (req, res)=>{
     const {nome, preco} = req.body
     console.log({nome, preco})
     res.send({nome, preco})
 })
+
+app.post('/enviar', (req, res)=>{
+    const {name, lastName} = req.body
+    console.log({name, lastName})
+    res.send()
+})
+
 
 
 app.listen(porta, function(){
